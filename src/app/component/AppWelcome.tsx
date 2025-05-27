5649138e4a723f4c014d54ef6f4e17e67f127c20
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-05-27 15:05:06 +0700

    add.env

--- src/app/component/AppWelcome.tsx
+++ src/app/component/AppWelcome.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Button } from "@/components/ui/button"
 import { useState } from "react"
 
 type AppWelcomeProps = {
@@ -19,7 +20,9 @@ export default function AppWelcome({headTitle,isShow}:AppWelcomeProps,) {
   return (
     <div>
       <h1>{headTitle}</h1>
+      <p>{process.env.NEXT_PUBLIC_APP_NAME}</p>
       <p>{title.toUpperCase()}</p>
+      <Button onClick={handleClick}>Click Me</Button>
       <button className="bg-blue-700 p-3 m-3 text-white rounded-lg" onClick={handleClick}>กดได้เลย!</button>
       {currentYear}
       {
