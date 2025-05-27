e2cfed93df68868abf72c2a79972b00cbb3f8c95
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-05-27 11:30:59 +0700

    update

--- src/app/component/AppWelcome.tsx
+++ src/app/component/AppWelcome.tsx
@@ -1,3 +1,24 @@
+'use client'
+
 export default function AppWelcome() {
-    
+  const title = 'Welcome COSCI'
+  const currebtYear = <p>2025</p>
+  const isShow = true;
+  const handleClick = () => {
+    alert('Hello Typescipt');
+  }
+
+  return (
+    <div>
+      <p>{title.toUpperCase()}</p>
+      <button onClick={handleClick}>กดได้เลย!</button>
+      {currebtYear}
+      {
+        isShow && <p>Date: 10/10/1998</p>
+      }
+      {
+        isShow ? <p>Hello Next.js</p> : <p>Hello JS</p>
+      }
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
+import AppWelcome from "./component/AppWelcome";
+
+
 export default function Home() {
-  const title = 'Welcome COSCI'
-  const currebtYear = <p>2025</p>
-  const isShow = true;
   return (
     <div>
-      <p>{title.toUpperCase()}</p>
-      {currebtYear}
-      {
-        isShow && <p>Date: 10/10/1998</p>
-      }
-      {
-        isShow ? <p>Hello Next.js</p> : <p>Hello JS</p>
-      }
+      <h1>Page</h1>
+      <AppWelcome />
     </div>
   );
 }
