847a054a4e09a314f35e4df1c841841bbf2cd00d
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-05-27 15:49:04 +0700

    add product and about

---
+++ src/app/product/page.tsx
@@ -0,0 +1,9 @@
+export default function Product () {
+    return(
+        <div className="m-5 p-5 border-2 rounded-md">
+            <h1>Product Page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, necessitatibus.</p>
+        </div>
+        
+    )
+}
