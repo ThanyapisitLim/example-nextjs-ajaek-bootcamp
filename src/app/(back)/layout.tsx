22e547a7cc76831139b782054495be90a7295368
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-05-28 13:26:33 +0700

    add dashboard

---
+++ src/app/(back)/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { K2D } from "next/font/google";
+import "../globals.css";
+
+const k2d = K2D({
+  subsets: ['thai'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+  display: 'swap',
+})
+
+export const metadata: Metadata = {
+  title: "ระบบขายสินค้า COSCI Shop",
+  description: "ขายสินค้าของนักศึกษาประจำปี 2568",
+};
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${k2d.className}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
