89e8a04321fee1eac0dc833723fe2a61b99db833
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-05-28 10:01:22 +0700

    add Navbar01page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { K2D } from "next/font/google";
 import "./globals.css";
-import AppNavbar from "./component/AppNavbar";
+import Navbar01Page from "@/components/navbar-01/navbar-01";
 
 const k2d = K2D({
   subsets: ['thai'],
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${k2d.className}`}>
-        <AppNavbar />
+        <Navbar01Page />
         <hr />
         {children}
       </body>
