9b51c35e0d6b1ce3e527bcd64af2bb1cd858c14a
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-05-28 15:32:31 +0700

    add Img by API

--- src/app/(front)/course/page.tsx
+++ src/app/(front)/course/page.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Features05Page from "@/components/features-05/features-05";
 
-export default function Course() {
+export default async function Course() {
+    const data = await fetch('https://api.codingthailand.com/api/course',{cache:'no-store'})
+    const courses = await data.json()
   return (
     <>
-        <Features05Page />
+    {
+      courses &&  <Features05Page courses = {courses.data} />
+    }
     </>
   );
 }
