f67f143795bd23cf88e8d5b8dfb47f4f9b72b3e7
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-05-28 14:27:27 +0700

    add course

--- src/components/footer-05/footer-05.tsx
+++ src/components/footer-05/footer-05.tsx
@@ -1,4 +1,3 @@
-import { Separator } from "@/components/ui/separator";
 import {
   DribbbleIcon,
   GithubIcon,
@@ -73,7 +72,6 @@ const Footer05Page = () => {
               ))}
             </ul>
           </div>
-          <Separator />
           <div className="py-8 flex flex-col-reverse sm:flex-row items-center justify-between gap-x-2 gap-y-5 px-6 xl:px-0">
             {/* Copyright */}
             <span className="text-muted-foreground">
