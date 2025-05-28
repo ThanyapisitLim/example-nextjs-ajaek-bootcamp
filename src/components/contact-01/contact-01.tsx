b4844b3f5c520b37e75113073e5e9be224cad078
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-05-28 14:13:04 +0700

    add API

--- src/components/contact-01/contact-01.tsx
+++ src/components/contact-01/contact-01.tsx
@@ -1,7 +1,10 @@
 import { MailIcon, MapPinIcon, PhoneIcon } from "lucide-react";
 import Link from "next/link";
+type Contact01PageProps = {
+  version: string
+}
 
-const Contact01Page = () => (
+const Contact01Page = ({version}:Contact01PageProps) => (
   <div className="mt-50 flex items-center justify-center">
     <div className="text-center">
       <b className="text-muted-foreground">Contact Us</b>
@@ -9,7 +12,7 @@ const Contact01Page = () => (
         Get In Touch
       </h2>
       <p className="mt-4 text-base sm:text-lg">
-        Our friendly team is always here to chat.
+        API Version: {version}
       </p>
       <div className="max-w-screen-xl mx-auto py-24 grid md:grid-cols-2 lg:grid-cols-3 gap-16 md:gap-10 px-6 md:px-0">
         <div className="text-center flex flex-col items-center">
