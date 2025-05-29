9bac02a7ec7d9a084ea36b71a6a1767d29154a80
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-05-29 13:39:08 +0700

    add Cart btm

---
+++ src/app/(front)/component/CartButton.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'user client'
+import { Button } from "@/components/ui/button";
+import { useCartStore } from "@/lib/cart-store";
+import { ChevronRight } from "lucide-react";
+
+export default function CartButton({ product }: any) {
+    const addItem = useCartStore((state) => state.addItem)
+    const handleAppItem = () => {
+        addItem({
+            productId: product.id,
+            title: product.title,
+            price: product.price,
+            qty:1
+        })
+    }
+    return (
+        <>
+            <Button className="mt-6 shadow-none" onClick={handleAppItem}>
+                Add to Cart <ChevronRight />
+            </Button>
+        </>
+    );
+}
